13c0abf27f454f3afb7f70d4853f79a93f3d24ce
Xu Chen <xu.chen@dal.ca>
2018-09-29 21:53:42 -0300

    update 'download_data.sh'

--- download_data.sh
+++ download_data.sh
@@ -5,8 +5,7 @@ rm -r $DATA_DIR
 
 wget https://storage.googleapis.com/capsule_toronto/mnist_data.tar.gz -P $DATA_DIR
 tar xvzf $DATA_DIR/mnist_data.tar.gz -C $DATA_DIR
-wget https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz -P
-$DATA_DIR
+wget https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz -P $DATA_DIR
 tar xvzf $DATA_DIR/cifar-10-binary.tar.gz -C $DATA_DIR
 
 
@@ -15,6 +14,5 @@ rm -r $CKPT_DIR
 
 wget https://storage.googleapis.com/capsule_toronto/mnist_checkpoints.tar.gz -P $CKPT_DIR
 tar xvzf $CKPT_DIR/mnist_checkpoints.tar.gz -C $CKPT_DIR
-wget https://storage.googleapis.com/capsule_toronto/cifar_checkpoints.tar.gz -P
-$CKPT_DIR
+wget https://storage.googleapis.com/capsule_toronto/cifar_checkpoints.tar.gz -P $CKPT_DIR
 tar xvzf $CKPT_DIR/cifar_checkpoints.tar.gz -C $CKPT_DIR
