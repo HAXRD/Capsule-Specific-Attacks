5a8ee80a763290f2d8508756018fef18fbc525de
Xu Chen <xu.chen@dal.ca>
2019-02-26 17:12:34 -0400

    Update scripts

--- scripts/caps_full/train/cifar10.sh
+++ scripts/caps_full/train/cifar10.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:2        # request GPU "generic resource"
 #SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=10000M        # memory per node
-#SBATCH --time=00-08:00      # time (DD-HH:MM)
+#SBATCH --time=00-04:00      # time (DD-HH:MM)
 #SBATCH --output=train/outs/cifar10-%N-%j.out  # %N for node name, %j for jobID
 
 source ~/tfp363/bin/activate
@@ -11,7 +11,7 @@ REPO_DIR=/home/xuc/DeepDream
 TOTAL_BATCH_SIZE=200
 MODEL=cap
 DATASET=cifar10
-MAX_EPOCHS=500
+MAX_EPOCHS=400
 SUMMARY_DIR=/home/xuc/projects/def-sageev/xuc/$MODEL/$DATASET
 
 python $REPO_DIR/experiment.py --total_batch_size=$TOTAL_BATCH_SIZE --model=$MODEL --data_dir=$REPO_DIR/data/$DATASET/ --dataset=$DATASET --max_epochs=$MAX_EPOCHS --summary_dir=$SUMMARY_DIR
